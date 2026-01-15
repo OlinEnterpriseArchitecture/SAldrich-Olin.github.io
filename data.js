4bd595651062ecd49b5274dc9a6dd45b19eacf6f
SAldrich-Olin <SAldrich@olin.com>
2026-01-15 13:41:54 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,670 @@
+const ORG_DATA = {
+    'All': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 832,
+                missingMandatory: 402,
+                missingQuality: 415,
+                missingApprovals: 378,
+                missingSurveys: 478
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 825,
+                missingMandatory: 370,
+                missingQuality: 405,
+                missingApprovals: 372,
+                missingSurveys: 459
+            }
+        ]
+    },
+    'IT / ERP': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 79,
+                missingMandatory: 35,
+                missingQuality: 29,
+                missingApprovals: 47,
+                missingSurveys: 59
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 87,
+                missingMandatory: 42,
+                missingQuality: 45,
+                missingApprovals: 57,
+                missingSurveys: 67
+            }
+        ]
+    },
+    'IT / HRIT': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 35,
+                missingMandatory: 18,
+                missingQuality: 17,
+                missingApprovals: 7,
+                missingSurveys: 16
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 33,
+                missingMandatory: 18,
+                missingQuality: 18,
+                missingApprovals: 7,
+                missingSurveys: 16
+            }
+        ]
+    },
+    'IT / I&O': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 161,
+                missingMandatory: 82,
+                missingQuality: 101,
+                missingApprovals: 61,
+                missingSurveys: 85
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 160,
+                missingMandatory: 65,
+                missingQuality: 99,
+                missingApprovals: 62,
+                missingSurveys: 84
+            }
+        ]
+    },
+    'IT / Security': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 38,
+                missingMandatory: 5,
+                missingQuality: 6,
+                missingApprovals: 5,
+                missingSurveys: 5
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 38,
+                missingMandatory: 5,
+                missingQuality: 7,
+                missingApprovals: 4,
+                missingSurveys: 4
+            }
+        ]
+    },
+    'IT / Winchester IT': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 296,
+                missingMandatory: 201,
+                missingQuality: 203,
+                missingApprovals: 174,
+                missingSurveys: 194
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 297,
+                missingMandatory: 194,
+                missingQuality: 196,
+                missingApprovals: 174,
+                missingSurveys: 194
+            }
+        ]
+    },
+    'IT / M&E': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 204,
+                missingMandatory: 50,
+                missingQuality: 40,
+                missingApprovals: 69,
+                missingSurveys: 106
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 205,
+                missingMandatory: 47,
+                missingQuality: 40,
+                missingApprovals: 70,
+                missingSurveys: 96
+            }
+        ]
+    },
+    'Site: EALT/OXFD': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 64,
+                missingMandatory: 30,
+                missingQuality: 32,
+                missingApprovals: 17,
+                missingSurveys: 27
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 61,
+                missingMandatory: 23,
+                missingQuality: 24,
+                missingApprovals: 15,
+                missingSurveys: 25
+            }
+        ]
+    },
+    'Site: LCAAP': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 190,
+                missingMandatory: 120,
+                missingQuality: 123,
+                missingApprovals: 113,
+                missingSurveys: 118
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 190,
+                missingMandatory: 119,
+                missingQuality: 122,
+                missingApprovals: 113,
+                missingSurveys: 118
+            }
+        ]
+    },
+    'IT / Winchester IT / BK&CD': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 108,
+                missingMandatory: 79,
+                missingQuality: 77,
+                missingApprovals: 62,
+                missingSurveys: 75
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 110,
+                missingMandatory: 72,
+                missingQuality: 70,
+                missingApprovals: 62,
+                missingSurveys: 75
+            }
+        ]
+    },
+    'IT / I&O / DNS&CI': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 56,
+                missingMandatory: 30,
+                missingQuality: 40,
+                missingApprovals: 21,
+                missingSurveys: 32
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 53,
+                missingMandatory: 25,
+                missingQuality: 35,
+                missingApprovals: 21,
+                missingSurveys: 30
+            }
+        ]
+    },
+    'IT / I&O / EUC': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 76,
+                missingMandatory: 32,
+                missingQuality: 46,
+                missingApprovals: 34,
+                missingSurveys: 42
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 77,
+                missingMandatory: 28,
+                missingQuality: 49,
+                missingApprovals: 33,
+                missingSurveys: 42
+            }
+        ]
+    }
+};
+
+// CMMC Applications Data
+const ORG_DATA_CMMC = {
+    'All': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 192,
+                missingMandatory: 66,
+                missingQuality: 76,
+                missingApprovals: 68,
+                missingSurveys: 65
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 193,
+                missingMandatory: 67,
+                missingQuality: 77,
+                missingApprovals: 69,
+                missingSurveys: 67
+            }
+        ]
+    },
+    'IT / ERP': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 0,
+                missingMandatory: 0,
+                missingQuality: 0,
+                missingApprovals: 0,
+                missingSurveys: 0
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 0,
+                missingMandatory: 0,
+                missingQuality: 0,
+                missingApprovals: 0,
+                missingSurveys: 0
+            }
+        ]
+    },
+    'IT / HRIT': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 0,
+                missingMandatory: 0,
+                missingQuality: 0,
+                missingApprovals: 0,
+                missingSurveys: 0
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 0,
+                missingMandatory: 0,
+                missingQuality: 0,
+                missingApprovals: 0,
+                missingSurveys: 0
+            }
+        ]
+    },
+    'IT / I&O': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 23,
+                missingMandatory: 1,
+                missingQuality: 1,
+                missingApprovals: 2,
+                missingSurveys: 1
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 24,
+                missingMandatory: 2,
+                missingQuality: 4,
+                missingApprovals: 4,
+                missingSurveys: 3
+            }
+        ]
+    },
+    'IT / Security': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 22,
+                missingMandatory: 0,
+                missingQuality: 2,
+                missingApprovals: 0,
+                missingSurveys: 0
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 21,
+                missingMandatory: 0,
+                missingQuality: 1,
+                missingApprovals: 0,
+                missingSurveys: 0
+            }
+        ]
+    },
+    'IT / Winchester IT': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 142,
+                missingMandatory: 66,
+                missingQuality: 75,
+                missingApprovals: 67,
+                missingSurveys: 65
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 142,
+                missingMandatory: 66,
+                missingQuality: 73,
+                missingApprovals: 66,
+                missingSurveys: 65
+            }
+        ]
+    },
+    'IT / M&E': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 4,
+                missingMandatory: 0,
+                missingQuality: 0,
+                missingApprovals: 0,
+                missingSurveys: 0
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 4,
+                missingMandatory: 0,
+                missingQuality: 0,
+                missingApprovals: 0,
+                missingSurveys: 0
+            }
+        ]
+    },
+    'Site: EALT/OXFD': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 67,
+                missingMandatory: 0,
+                missingQuality: 8,
+                missingApprovals: 6,
+                missingSurveys: 0
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 67,
+                missingMandatory: 0,
+                missingQuality: 8,
+                missingApprovals: 6,
+                missingSurveys: 1
+            }
+        ]
+    },
+    'Site: LCAAP': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 129,
+                missingMandatory: 65,
+                missingQuality: 68,
+                missingApprovals: 61,
+                missingSurveys: 64
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 129,
+                missingMandatory: 65,
+                missingQuality: 68,
+                missingApprovals: 61,
+                missingSurveys: 64
+            }
+        ]
+    }
+};
+
+// Non-CMMC Applications Data
+const ORG_DATA_NONCMMC = {
+    'All': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 640,
+                missingMandatory: 336,
+                missingQuality: 339,
+                missingApprovals: 310,
+                missingSurveys: 413
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 632,
+                missingMandatory: 303,
+                missingQuality: 328,
+                missingApprovals: 303,
+                missingSurveys: 392
+            }
+        ]
+    },
+    'IT / ERP': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 79,
+                missingMandatory: 35,
+                missingQuality: 29,
+                missingApprovals: 47,
+                missingSurveys: 59
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 87,
+                missingMandatory: 42,
+                missingQuality: 45,
+                missingApprovals: 57,
+                missingSurveys: 67
+            }
+        ]
+    },
+    'IT / HRIT': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 35,
+                missingMandatory: 18,
+                missingQuality: 17,
+                missingApprovals: 7,
+                missingSurveys: 16
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 33,
+                missingMandatory: 18,
+                missingQuality: 18,
+                missingApprovals: 7,
+                missingSurveys: 16
+            }
+        ]
+    },
+    'IT / I&O': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 138,
+                missingMandatory: 81,
+                missingQuality: 100,
+                missingApprovals: 59,
+                missingSurveys: 84
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 136,
+                missingMandatory: 63,
+                missingQuality: 95,
+                missingApprovals: 58,
+                missingSurveys: 81
+            }
+        ]
+    },
+    'IT / Security': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 16,
+                missingMandatory: 5,
+                missingQuality: 4,
+                missingApprovals: 5,
+                missingSurveys: 5
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 16,
+                missingMandatory: 5,
+                missingQuality: 6,
+                missingApprovals: 4,
+                missingSurveys: 4
+            }
+        ]
+    },
+    'IT / Winchester IT': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 154,
+                missingMandatory: 135,
+                missingQuality: 129,
+                missingApprovals: 107,
+                missingSurveys: 129
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 155,
+                missingMandatory: 128,
+                missingQuality: 123,
+                missingApprovals: 108,
+                missingSurveys: 129
+            }
+        ]
+    },
+    'IT / M&E': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 200,
+                missingMandatory: 50,
+                missingQuality: 40,
+                missingApprovals: 69,
+                missingSurveys: 106
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 201,
+                missingMandatory: 47,
+                missingQuality: 40,
+                missingApprovals: 70,
+                missingSurveys: 96
+            }
+        ]
+    },
+    'Site: EALT/OXFD': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 45,
+                missingMandatory: 30,
+                missingQuality: 27,
+                missingApprovals: 11,
+                missingSurveys: 27
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 45,
+                missingMandatory: 23,
+                missingQuality: 21,
+                missingApprovals: 11,
+                missingSurveys: 26
+            }
+        ]
+    },
+    'Site: LCAAP': {
+        startDate: '2026-01-02',
+        targetDate: '2026-04-03',
+        weeklyProgress: [
+            {
+                date: '2026-01-02',
+                totalApplications: 61,
+                missingMandatory: 55,
+                missingQuality: 55,
+                missingApprovals: 52,
+                missingSurveys: 54
+            },
+            {
+                date: '2026-01-09',
+                totalApplications: 61,
+                missingMandatory: 54,
+                missingQuality: 55,
+                missingApprovals: 52,
+                missingSurveys: 54
+            }
+        ]
+    }
+};
