4b00a46cdb4ce68093b11ab7ab4c622ce6151597
SAldrich-Olin <SAldrich@olin.com>
2026-01-16 13:48:26 -0600

    Auto-update from Power BI - 2026-01-16T19:48:26.0353016Z

---
+++ data2.js
@@ -0,0 +1,14 @@
+// Auto-generated from Power BI
+// Last updated: 2026-01-16T19:48:26.0243619Z
+
+const data = {
+    totalApplications: 819,
+    missingMandatoryFields: 348,
+    missingQualitySeal: 387,
+    missingApprovals: 365,
+    missingSurveys: 405,
+    lastUpdated: "2026-01-16T19:48:26.0243815Z",
+    timestamp: "2026-01-16T19:48:26.0243832Z"
+};
+
+export default data;
