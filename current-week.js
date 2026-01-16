22dbb61a87c59c9b316b3f63ca57f5fba523b102
SAldrich-Olin <SAldrich@olin.com>
2026-01-16 15:18:29 -0600

    Update current-week.js from Power BI - 2026-01-16T21:18:29.3653533Z

--- current-week.js
+++ current-week.js
@@ -1,8 +1,8 @@
 // Auto-generated from Power BI
-// Last updated: 2026-01-16T21:09:18.0226095Z
+// Last updated: 2026-01-16T21:18:29.1064890Z
 
 const CURRENT_WEEK_DATA = {
-    date: "2026-01-16T21:09:18.0226133Z",  // Closest Friday
+    date: "2026-01-16T21:18:29.1064928Z",  // Closest Friday
     all: {
         'All': {
             totalApplications: 819,
