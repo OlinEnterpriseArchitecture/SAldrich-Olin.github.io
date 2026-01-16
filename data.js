011fcd6ca2f3d3472a06009ddec1bc167104fed2
SAldrich-Olin <SAldrich@olin.com>
2026-01-16 15:56:22 -0600

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,25 @@
 // Application Completion Tracking Data
-// Last updated: 2026-01-16T21:53:14.608Z
+// Last updated: 2026-01-16T21:48:46.098Z
 // Target week: 2026-01-23
 // Cutoff: Friday at 17:00 UTC
 
 // All Applications (CMMC + Non-CMMC)
-const ORG_DATA = undefined;
+const ORG_DATA = {
+    "All": {
+        "weeklyProgress": []
+    }
+};
+
+// CMMC Applications Data
+const ORG_DATA_CMMC = {
+    "All": {
+        "weeklyProgress": []
+    }
+};
+
+// Non-CMMC Applications Data
+const ORG_DATA_NONCMMC = {
+    "All": {
+        "weeklyProgress": []
+    }
+};
